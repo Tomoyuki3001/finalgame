36d051e1728834115bf894b016d3aca94316b7ff
Tomoyuki Fujii <mjr01300828@gmail.com>
2022-04-05 15:51:14 -0700

    I made timer

---
+++ JavaScript/game_hard.js
@@ -0,0 +1,25 @@
+//Timer function
+
+const timeH = document.querySelector("h1");
+let timesecond = 120;
+
+displayTime(timesecond);
+
+const countdown = setInterval(() => {
+  timesecond--;
+  displayTime(timesecond);
+  if (timesecond <= 0 || timesecond < 1) {
+    endtime();
+    clearInterval(countdown);
+  }
+}, 1000);
+
+function displayTime(second) {
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60);
+  timeH.innerHTML = `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`;
+}
+
+function endtime() {
+  timeH.innerHTML = "Time out";
+}
